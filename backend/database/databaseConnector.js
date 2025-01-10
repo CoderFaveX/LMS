27a4d69208a24f596a6a79bf8ce7bdcf7f935acd
CoderFaveX <faveejiofor2009@gmail.com>
2025-01-10 04:39:32 +0100

    mogodb

--- backend/database/databaseConnector.js
+++ backend/database/databaseConnector.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const ConnectDatabase = (connection_url) => {
-  mongoose.connect(connection_url)
-}
+  mongoose.connect(connection_url, { dbName: "librarymgmtsystem" });
+};
 
-module.exports = { ConnectDatabase }
+module.exports = { ConnectDatabase };
